91fec84d34f4060201ea6aba63e844408428651e
Adesh Srivastava <adeshsrivastava.ofc@gmail.com>
2026-01-30 12:52:18 +0530

    Getting Started with Spring Boot

---
+++ src/main/java/com/adeshsrivastava/store/StoreApplication.java
@@ -0,0 +1,14 @@
+package com.adeshsrivastava.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+        System.out.println("Store Application Started");
+    }
+
+}
